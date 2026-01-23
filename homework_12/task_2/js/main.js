f61d0d9a3b122deb760552832e3126e4a1867055
Виталий Александрович Андрющенко <andryushchenkovitaliy@gmail.com>
2026-01-23 21:30:32 +0200

    Delete homework_12 directory

--- homework_12/task_2/js/main.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-const text = document.getElementById('text');
-const btn = document.getElementById('btn');
-
-btn.addEventListener('click', () => {
-  text.classList.toggle('active');
-});
