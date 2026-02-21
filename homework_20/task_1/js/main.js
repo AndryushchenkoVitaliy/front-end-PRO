7b924999f63cd3d3ac135c3f564c2fe4f0cc0c38
Виталий Александрович Андрющенко <andryushchenkovitaliy@gmail.com>
2026-02-21 13:03:19 +0200

    add homework_20

---
+++ homework_20/task_1/js/main.js
@@ -0,0 +1,32 @@
+'use strict';
+
+$(document).ready(function () {
+
+    const modal = new bootstrap.Modal($('#taskModal')[0]);
+
+    $('#addBtn').on('click', function () {
+        const text = $('#taskInput').val().trim();
+        if (!text) return;
+
+        $('#list').append(`
+        <li>
+          ${text}
+          <button class="delete">Видалити</button>
+        </li>
+      `);
+
+        $('#taskInput').val('');
+    });
+
+    $('#list').on('click', '.delete', function (e) {
+        e.stopPropagation();
+        $(this).parent().remove();
+    });
+
+    $('#list').on('click', 'li', function () {
+        const text = $(this).clone().children().remove().end().text().trim();
+        $('#modalTaskText').text(text);
+        modal.show();
+    });
+
+});
